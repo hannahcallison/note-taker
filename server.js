13c6a53f9874e9e9f38efc2e67db5dfb77684058
Hannah Callison <hannahcallison1007@gmail.com>
2022-04-29 14:04:33 -0700

    bug fixing

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// GET route for home page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
@@ -28,58 +22,7 @@ app.get('/api/notes', (req, res) => {
   return res.json(notesDB);
 });
 
-app.post('/api/notes', (req, res) =>{
- // Log that a POST request was received
- console.info(`${req.method} request received`);
 
- // Destructuring assignment for the items in req.body
- const { title, text } = req.body;
-
- // If all the required properties are present
- if (title && text) {
-   // Variable for the object we will save
-   const newNote = {
-     title,
-     text,
-     id: uuid(),
-   };
-
-   fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-      const notes = JSON.parse(data);
-      console.log(req.body);
-      notes.push(newNote);
-      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
-      err
-        ? console.error(err)
-        : console.log('New note has been written to JSON file')
-      )
-   })
-   const response = {
-     status: 'success',
-     body: newNote,
-   };
-   console.log(response);
-   res.status(201).json(response);
- } else {
-   res.status(500).json('Error');
- }
-})
-
-app.delete("/api/notes/:id", (req, res) => {
-  console.log(req.method, req.url);
-  res.send("delete requested");
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-      const notes = JSON.parse(data);
-      const noteDelete = notes.filter((notes) => notes.id != req.params.id);
-      const noteStr = JSON.stringify(noteDelete);
-      console.log(noteStr);
-      fs.writeFile("./db/db.json", noteStr, (err) =>
-          err 
-          ? console.error(err) 
-          : console.log(`Note deleted`)
-      );
-  });
-});
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
